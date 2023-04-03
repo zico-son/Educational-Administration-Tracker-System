bb931aed329b26371e016cf320af6fca9fc08436
Zicos <zeko.son@gmail.com>
2023-04-03 19:33:46 +0200

    extend auth user admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+# Registering models with the admin site for testing purposes
 
-# Register your models here.
+class UserAdmin (BaseUserAdmin):
+    pass
