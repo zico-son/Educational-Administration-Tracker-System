7b8aefd0a0ee7338aff2b40c6a0b6365869bbc29
Ahmed Zakaria <zicocomlimited@gmail.com>
2023-04-14 13:49:21 +0200

    register main models for testing

--- EvaluateHub/admin.py
+++ EvaluateHub/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from EvaluateHub.models import *
 
-# Register your models here.
+@admin.register(EvaluationForm)
+class EvaluationFormAdmin(admin.ModelAdmin):
+    pass
+@admin.register(StudentsAffairs)
+class StudentsAffairsAdmin(admin.ModelAdmin):
+    pass
+@admin.register(WorkersAffairs)
+class WorkersAffairsAdmin(admin.ModelAdmin):
+    pass
