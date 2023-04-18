2c2427cd6a1a5147006f3e685a2217da87b5d19c
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-18 16:40:22 +0200

    add EvaluationForm endpoint

--- EvaluateHub/urls.py
+++ EvaluateHub/urls.py
@@ -4,7 +4,7 @@ from EvaluateHub.views import *
 
 router = routers.DefaultRouter()
 router.register('students-affairs', StudentsAffairsViewSet, basename='students-affairs')
-
+router.register('evaluation-form', EvaluationFormViewSet, basename='evaluation-form')
 urlpatterns = [
     path('', include(router.urls)),
 ]
