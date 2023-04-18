1598ab4d9779c99528a6b83ad27eddcf3fec0db1
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-18 16:44:16 +0200

    add EvaluationForm Serializer and create method

--- EvaluateHub/serializers.py
+++ EvaluateHub/serializers.py
@@ -85,3 +85,77 @@ class EnvironmentPopulationSerializer(ModelSerializer):
         model = EnvironmentPopulation
         fields = '__all__'
 
+class EvaluationFormSerializer(ModelSerializer):
+    students_affairs = StudentsAffairsSerializer()
+    security_safety = SecuritySafetySerializer()
+    teachers = TeachersSerializer()
+    workers_affairs = WorkersAffairsSerializer()
+    strategic_planning = StrategicPlanningSerializer()
+    administration = AdministrationSerializer()
+    training = TrainingSerializer()
+    nutrition = NutritionSerializer()
+    cooperative = CooperativeSerializer()
+    laboratories = LaboratoriesSerializer()
+    decentralization = DecentralizationSerializer()
+    production_unit = ProductionUnitSerializer()
+    environment_population = EnvironmentPopulationSerializer()
+    quality = QualitySerializer()
+
+    class Meta:
+        model = EvaluationForm
+        fields = ['id', 'school_name', 'school_id', 'school_level','security_safety', 'teachers','students_affairs', 'workers_affairs', 'strategic_planning', 'administration', 'training', 'nutrition', 'cooperative', 'laboratories', 'decentralization', 'production_unit', 'environment_population', 'quality', 'security_safety', 'teachers']
+    def create(self, validated_data):
+        students_affairs_data = validated_data.pop('students_affairs')
+        first_class_data = students_affairs_data.pop('first_class')
+        first_class = ClassRecord.objects.create(**first_class_data)
+        students_affairs =StudentsAffairs.objects.create( first_class=first_class, **students_affairs_data)
+
+        security_safety_data =validated_data.pop('security_safety')
+        security_factors_data = security_safety_data.pop('security_factors')
+        security_factors = SecurityFactors.objects.create(**security_factors_data)
+        security_safety = SecuritySafety.objects.create(security_factors= security_factors,**security_safety_data)
+
+        teachers_data = validated_data.pop('teachers')
+        material_one_data = teachers_data.pop('material_one')
+        material_one = Material.objects.create(**material_one_data)
+        teachers = Teachers.objects.create(material_one = material_one, **teachers_data)
+
+        workers_affairs_data = validated_data.pop('workers_affairs')
+        workers_affairs = WorkersAffairs.objects.create(**workers_affairs_data)
+
+        strategic_planning_data = validated_data.pop('strategic_planning')
+        strategic_planning = StrategicPlanning.objects.create(**strategic_planning_data)
+        
+        administration_data = validated_data.pop('administration')
+        administration = Administration.objects.create(**administration_data)
+        
+        training_data = validated_data.pop('training')
+        training = Training.objects.create(**training_data)
+        
+        nutrition_data = validated_data.pop('nutrition')
+        nutrition = Nutrition.objects.create(**nutrition_data)
+        
+        cooperative_data = validated_data.pop('cooperative')    
+        cooperative = Cooperative.objects.create(**cooperative_data)
+        
+        laboratories_data = validated_data.pop('laboratories')
+        laboratories = Laboratories.objects.create(**laboratories_data)
+        
+        decentralization_data = validated_data.pop('decentralization')
+        decentralization = Decentralization.objects.create(**decentralization_data)
+        
+        production_unit_data = validated_data.pop('production_unit')
+        production_unit = ProductionUnit.objects.create(**production_unit_data)
+        
+        environment_population_data = validated_data.pop('environment_population')
+        environment_population = EnvironmentPopulation.objects.create(**environment_population_data)
+        
+        quality_data = validated_data.pop('quality')
+        quality = Quality.objects.create(**quality_data)
+
+        user_id =self.context['user_id']
+        print (user_id)
+        created_by = User.objects.get(pk=user_id)
+
+        evaluation_form = EvaluationForm.objects.create(students_affairs=students_affairs, workers_affairs=workers_affairs, strategic_planning=strategic_planning, administration=administration, training=training, nutrition=nutrition, cooperative=cooperative, laboratories=laboratories, decentralization=decentralization, production_unit=production_unit, environment_population=environment_population, quality=quality,security_safety=security_safety,teachers = teachers, created_by =created_by,**validated_data)
+        return evaluation_form
