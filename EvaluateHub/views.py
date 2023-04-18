467b6f9356756b1b8aa67e9991cb5c5f13ebfb82
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-18 16:39:25 +0200

    add EvaluationFormViewSet

--- EvaluateHub/views.py
+++ EvaluateHub/views.py
@@ -7,3 +7,14 @@ from EvaluateHub.serializers import *
 class StudentsAffairsViewSet(ModelViewSet):
     queryset = StudentsAffairs.objects.all()
     serializer_class = StudentsAffairsSerializer
+
+class EvaluationFormViewSet(ModelViewSet):
+
+    def get_queryset(self):
+        return EvaluationForm.objects.all()
+    
+    def get_serializer_class(self):
+        return EvaluationFormSerializer
+    
+    def get_serializer_context(self):
+        return {'user_id': self.request.user.id}
