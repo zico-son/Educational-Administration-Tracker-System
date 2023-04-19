503f322f4e7d0087317c2b488461fc7ac253d25c
Ahmed Zakaria <zeko.son@gmail.com>
2023-04-19 06:00:29 +0200

    add CORS

--- FMS/settings.py
+++ FMS/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     'djoser',
     'rest_framework',
     'core',
@@ -44,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -54,6 +56,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'FMS.urls'
+
+CORS_ORIGIN_ALLOW_ALL = True # LIMIT IT WHEN DEPLOYING
 
 TEMPLATES = [
     {
