deb728658c81458508d17766a1460639f34131a8
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-25 14:15:35 +0200

    add IssuesHub

--- FMS/urls.py
+++ FMS/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('', include('core.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
+    path('', include('IssuesHub.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
