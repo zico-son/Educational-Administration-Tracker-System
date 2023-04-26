98491cd2a07f3dc7015747db11a209871f0d0d3b
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-26 07:52:43 +0200

    add create_response_if_not_empty

--- EvaluateHub/validators.py
+++ EvaluateHub/validators.py
@@ -1,3 +1,7 @@
 def create_issue_if_not_empty (value,IssueManager,IssueManagerObject):
     if value.get('issue') != "" :
         IssueManager.objects.create(department= IssueManagerObject, **value)
+
+def create_response_if_not_empty (value,ResponseManager,ResponseManagerObject):
+    if value.get('response') != "" :
+        ResponseManager.objects.create(department= ResponseManagerObject, **value)
