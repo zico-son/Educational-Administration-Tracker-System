68c8ef46cbc47e6f5e6123be1cc0e5f54647a715
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-26 07:15:03 +0200

    optimize EvaluationFormViewSet query

--- EvaluateHub/views.py
+++ EvaluateHub/views.py
@@ -11,7 +11,51 @@ class StudentsAffairsViewSet(ModelViewSet):
 class EvaluationFormViewSet(ModelViewSet):
 
     def get_queryset(self):
-        return EvaluationForm.objects.all()
+        return EvaluationForm.objects \
+        .select_related('students_affairs__first_class') \
+        .select_related('security_safety__security_factors') \
+        .select_related('teachers__material_one') \
+        .select_related('workers_affairs') \
+        .select_related('strategic_planning') \
+        .select_related('administration') \
+        .select_related('training') \
+        .select_related('nutrition') \
+        .select_related('cooperative') \
+        .select_related('laboratories') \
+        .select_related('decentralization') \
+        .select_related('production_unit') \
+        .select_related('environment_population') \
+        .select_related('quality') \
+        .select_related('students_affairs__issue') \
+        .select_related('security_safety__issue') \
+        .select_related('teachers__issue') \
+        .select_related('workers_affairs__issue') \
+        .select_related('strategic_planning__issue') \
+        .select_related('administration__issue') \
+        .select_related('training__issue') \
+        .select_related('nutrition__issue') \
+        .select_related('cooperative__issue') \
+        .select_related('laboratories__issue') \
+        .select_related('decentralization__issue') \
+        .select_related('production_unit__issue') \
+        .select_related('environment_population__issue') \
+        .select_related('quality__issue') \
+        .select_related('students_affairs__response') \
+        .select_related('security_safety__response') \
+        .select_related('teachers__response') \
+        .select_related('workers_affairs__response') \
+        .select_related('strategic_planning__response') \
+        .select_related('administration__response') \
+        .select_related('training__response') \
+        .select_related('nutrition__response') \
+        .select_related('cooperative__response') \
+        .select_related('laboratories__response') \
+        .select_related('decentralization__response') \
+        .select_related('production_unit__response') \
+        .select_related('environment_population__response') \
+        .select_related('quality__response') \
+        .select_related('students_affairs__response') \
+        .all()
     
     def get_serializer_class(self):
         return EvaluationFormSerializer
