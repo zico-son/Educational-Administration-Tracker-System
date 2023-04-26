44e271c0dabd354e15dfdf6821a29210388c93ac
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-27 01:53:21 +0200

    add NoPostViewSet

---
+++ EvaluateHub/custom_view_set.py
@@ -0,0 +1,5 @@
+from rest_framework.viewsets import GenericViewSet
+from rest_framework.mixins import ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+
+class NoPostViewSet(GenericViewSet, ListModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin):
+    pass
