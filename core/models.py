c8c29984d26a2a53ff90d64782aeea72512ed6dd
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-27 16:59:39 +0200

    add system admin

--- core/models.py
+++ core/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class User(BaseAbstractUser):
+    system_admin = 'system_admin'
     manager = 'manager'
     vice = 'vice'
     tracker = 'tracker'
@@ -23,6 +24,7 @@ class User(BaseAbstractUser):
     nutrition_admin = 'nutrition_admin'
 
     roles = [
+        (system_admin, 'system_admin'),
         (manager, 'manager'),
         (vice, 'vice'),
         (tracker, 'tracker'),
@@ -43,76 +45,3 @@ class User(BaseAbstractUser):
     ] 
     role = models.CharField(max_length=255, null=True, blank=True, choices= roles)
     email = models.EmailField(unique=True)
-
-    # A model that is the base for all users in the system.
-    # AbstractUser is a base class that can be used to create custom user models. It contains all the fields and behaviors that are required for a user model, but none of the specific fields and behaviors that are used for a typical user model.
-    # The only required field is username, which is used to authenticate the user. Other fields are optional, but recommended.
-
-    # Fields
-    # username
-    # password
-    # last_login
-    # is_superuser
-    # first_name
-    # last_name
-    # email
-    # is_staff
-    # is_active
-    # date_joined
-
-    # Methods
-    # get_full_name()
-    # get_short_name()
-    # set_password(raw_password)
-    # check_password(raw_password)
-    # set_unusable_password()
-    # has_usable_password()
-    # has_perm(perm, obj=None)
-    # has_perms(perm_list, obj=None)
-    # has_module_perms(app_label)
-    # get_group_permissions(obj=None)
-    # get_all_permissions(obj=None)
-    # has_perm(perm, obj=None)
-    # has_perms(perm_list, obj=None)
-    # has_module_perms(app_label)
-    # get_group_permissions(obj=None)
-    # get_all_permissions(obj=None)
-    # is_anonymous()
-    # is_authenticated()
-    # save(*args, **kwargs)
-    # delete(*args, **kwargs)
-    # __str__()
-    # __repr__()
-    # __eq__()
-    # __ne__()
-    # __hash__()
-    # __bool__()
-    # __unicode__()
-    # __format__()
-    # __getattribute__()
-    # __setattr__()
-    # __delattr__()
-    # __lt__()
-    # __le__()
-    # __gt__()
-    # __ge__()
-    # __init__()
-    # __new__()
-    # __reduce__()
-    # __reduce_ex__()
-    # __setstate__()
-    # __sizeof__()
-    # __dir__()
-    # __class__
-    # __dict__
-    # __doc__
-    # __module__
-    # __weakref__
-
-    # Meta
-    # abstract = True
-    # permissions = None
-    # verbose_name = 'user'
-    # verbose_name_plural = 'users'
-    # db_table = 'auth_user'
-    # swappable = 'AUTH_USER
