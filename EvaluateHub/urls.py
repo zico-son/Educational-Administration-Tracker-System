90b2f41683e798638bbccba4ec1e698f36c320fb
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-27 17:09:49 +0200

    add departments endpoints

--- EvaluateHub/urls.py
+++ EvaluateHub/urls.py
@@ -3,8 +3,21 @@ from rest_framework import routers
 from EvaluateHub.views import *
 
 router = routers.DefaultRouter()
-router.register('students-affairs', StudentsAffairsViewSet, basename='students-affairs')
 router.register('evaluation-form', EvaluationFormViewSet, basename='evaluation-form')
+router.register('students-affairs', StudentsAffairsViewSet, basename='students-affairs')
+router.register('security-safety', SecuritySafetyViewSet, basename='security-safety')
+router.register('teachers', TeachersViewSet, basename='teachers')
+router.register('workers-affairs', WorkersAffairsViewSet, basename='workers-affairs')
+router.register('strategic-planning', StrategicPlanningViewSet, basename='strategic-planning')
+router.register('administration', AdministrationViewSet, basename='administration')
+router.register('training', TrainingViewSet, basename='training')
+router.register('nutrition', NutritionViewSet, basename='nutrition')
+router.register('cooperative', CooperativeViewSet, basename='cooperative')
+router.register('decentralization', DecentralizationViewSet, basename='decentralization')
+router.register('production-unit', ProductionUnitViewSet, basename='production-unit')
+router.register('environment-population', EnvironmentPopulationViewSet, basename='environment-population')
+router.register('laboratories', LaboratoriesViewSet, basename='laboratories')
+router.register('quality', QualityViewSet, basename='quality')
 urlpatterns = [
     path('', include(router.urls)),
 ]
