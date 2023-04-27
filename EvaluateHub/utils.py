77cc35e8547106694d5010938e7cad287c0f25b4
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-27 17:11:05 +0200

    add permission exception handler

--- EvaluateHub/utils.py
+++ EvaluateHub/utils.py
@@ -12,3 +12,10 @@ def create_response_if_not_empty (value,ResponseManager,ResponseManagerObject):
 def update_response_if_not_empty (value,ResponseManager,ResponseManagerObject):
     if value.get('response') != "" :
         ResponseManager.objects.update(department= ResponseManagerObject, **value)
+
+
+def permission_exception_handler(request, role):
+    try:
+            return bool(request.user.role == role)
+    except:
+            return False
