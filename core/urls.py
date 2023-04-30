de54e50924103f08f35a42a345a03c79f1807197
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-04-30 03:14:58 +0300

    add roles endpoint

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView
-
+from .views import roles_view
 urlpatterns = [
-    path ('login/', TokenObtainPairView.as_view())
+    path ('login/', TokenObtainPairView.as_view()),
+    path ('roles/', roles_view),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+
+def roles_view(request):
+    system_admin = 'system_admin'
+    manager = 'manager'
+    vice = 'vice'
+    tracker = 'tracker'
+    students_affairs_admin = 'students_affairs_admin'
+    environment_population_admin = 'environment_population_admin'
+    production_unit_admin = 'production_unit_admin'
+    laboratories_admin = 'laboratories_admin'
+    cooperative_admin = 'cooperative_admin'
+    decentralization_admin = 'decentralization_admin'
+    workers_affairs_admin = 'workers_affairs_admin'
+    security_safety_admin = 'security_safety_admin'
+    teachers_admin = 'teachers_admin'
+    strategic_planning_admin = 'strategic_planning_admin'
+    administration_admin = 'administration_admin'
+    quality_admin = 'quality_admin'
+    training_admin = 'training_admin'
+    nutrition_admin = 'nutrition_admin'
+    roles = [
+        (system_admin, 'system_admin'),
+        (manager, 'manager'),
+        (vice, 'vice'),
+        (tracker, 'tracker'),
+        (students_affairs_admin, 'students_affairs_admin'),
+        (environment_population_admin, 'environment_population_admin'),
+        (production_unit_admin, 'production_unit_admin'),
+        (laboratories_admin, 'laboratories_admin'),
+        (cooperative_admin, 'cooperative_admin'),
+        (decentralization_admin, 'decentralization_admin'),
+        (workers_affairs_admin, 'workers_affairs_admin'),
+        (security_safety_admin, 'security_safety_admin'),
+        (teachers_admin, 'teachers_admin'),
+        (strategic_planning_admin, 'strategic_planning_admin'),
+        (administration_admin, 'administration_admin'),
+        (quality_admin, 'quality_admin'),
+        (training_admin, 'training_admin'),
+        (nutrition_admin, 'nutrition_admin'),
+    ]
+    roles_dict = dict(roles)
+    return JsonResponse(roles_dict)
