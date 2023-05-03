84673b8f9c16f77f70a7abdd91c47ee05c4fc92b
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-03 23:41:22 +0300

    update CustomPagination

--- EvaluateHub/pagination.py
+++ EvaluateHub/pagination.py
@@ -3,12 +3,3 @@ from rest_framework.response import Response
 class CustomPagination(PageNumberPagination):
 
     page_size = 10
-
-
-    def get_paginated_response(self, data):
-        return Response({
-            'next': self.page.next_page_number() if self.page.has_next() else None,
-            'previous': self.page.previous_page_number() if self.page.has_previous() else None,
-            'count': self.page.paginator.count,
-            'results': data
-        })
