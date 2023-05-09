a639f0d16b42c238883900055c9bb681f2ba99f3
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-09 11:32:56 +0300

    Import signals

--- EvaluateHub/apps.py
+++ EvaluateHub/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class EvaluatehubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'EvaluateHub'
+
+    def ready(self):
+        import EvaluateHub.signals
