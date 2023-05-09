a5ef167afe7f2c214163fdc78ee6cf49a464cc2a
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-05-09 11:30:00 +0300

    add user delete serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,7 @@
 from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 from djoser.serializers import UserSerializer as BaseUserSerializer
-
+from rest_framework import serializers
+from core.models import User
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password', 'email', 'first_name', 'last_name','role']
@@ -8,3 +9,8 @@ class UserCreateSerializer(BaseUserCreateSerializer):
 class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'role']
+
+
+class UserDeleteSerializer(serializers.Serializer):
+    class Meta:
+        model = User
