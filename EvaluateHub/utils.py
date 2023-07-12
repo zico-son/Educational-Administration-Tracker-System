6ec2977a43f8604338e8f9fcc87fb526c8225c4e
Ahmad Zakaria <77364329+zico-son@users.noreply.github.com>
2023-07-12 16:40:38 +0300

    add issue_checker, counter_checker

--- EvaluateHub/utils.py
+++ EvaluateHub/utils.py
@@ -4,6 +4,8 @@
 def create_issue_if_not_empty (value,IssueManager,IssueManagerObject):
     if value.get('issue') != "" :
         IssueManager.objects.create(department= IssueManagerObject, **value)
+        return True
+    return False
 
 def create_response_if_not_empty (value,ResponseManager,ResponseManagerObject):
     if value.get('response') != "" :
@@ -19,3 +21,14 @@ def permission_exception_handler(request, role):
             return bool(request.user.role == role)
     except:
             return False
+
+def issue_checker(issue, counter):
+    if issue is True:
+        counter += 1
+    return counter
+
+def counter_checker(counter):
+    if counter > 0:
+        return 1
+    else :
+        return 0
